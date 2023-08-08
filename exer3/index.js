c6a43a6aca9bf7aff6e386847e639a14b51e03c2
Lucas Santos <lucas22.ferreira.007@gmail.com>
2023-08-08 20:00:48 -0300

    feito o exer3

--- exer3/index.js
+++ exer3/index.js
@@ -1,4 +1,35 @@
+const individuos = []
 
+do{
+  const nome = prompt('Qual o seu nome?')
+  const idade = Number(prompt('Qual a sua idade?'))
+  const trabalho = confirm('Está trabalhando?')
+  let salario = 0
+    if(trabalho){
+      salario += parseFloat(prompt('Qual o seu salário?'))
+    }
+  individuos.push({
+    nome,
+    idade,
+    trabalho,
+    salario
+  })
+}while(confirm('Deseja cadastrar um novo indivíduo?'))
+
+  for(const individuo of individuos){
+    if(!individuo.trabalho){
+      document.write(`<p><strong>Pessoas desempregadas:</strong></p>`)
+      document.write(`<p>Nome: ${individuo.nome}, idade: ${individuo.idade};</p>`)
+    } else if(individuo.trabalho && individuo.salario < 2500){
+      document.write(`<p><strong>Pessoas empregadas com salários menores que 2500:</strong></p>`)
+      document.write(`<p>Nome: ${individuo.nome}, idade: ${individuo.idade},  salário: ${individuo.salario.toFixed(2)};</p>`)
+    } else if(individuo.trabalho && individuo.salario >= 2500){
+      document.write(`<p><strong>Pessoas empregadas com salários maiores que 2500:</strong></p>`)
+      document.write(`<p>Nome: ${individuo.nome}, idade: ${individuo.idade},  salário: ${individuo.salario.toFixed(2)};</p>`)
+    }else{
+      document.write('NENHUM VALOR INFORMADO!!!')
+    }
+  }
 
 // 3. Crie um cadastro de pessoas onde o usuário informe o nome, idade
 // e se está trabalhando ou não, se a pessoa estiver trabalhando
