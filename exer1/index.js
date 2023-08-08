518a883b618e3b62895b4ea077673e58fac740ca
Lucas Santos <lucas22.ferreira.007@gmail.com>
2023-08-08 19:40:33 -0300

    corrigido exer1

--- exer1/index.js
+++ exer1/index.js
@@ -1,8 +1,8 @@
 const salarios = [5000.00, 5460.50, 3452.00, 6900.00, 7590.10, 8012.99,
 1290.00, 15000.00]
 
-console.log(salarios.findIndex((item) => item>7500))
-console.log(salarios.filter((item) => item>8000))
+console.log(salarios.findIndex(item => item>7500))
+console.log(salarios.filter(item => item>8000))
 
 // 1. Considere o seguinte array:
 
